2962c5f9eb6f91ba47ab6f5bf31c87339d6ab545
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-05-06 18:46:31 +0100

    Make Twitter Keys Configurable at startup

--- src/main/scala/uk.co.bruntonspall.scalecamp/servlets/DispatcherServlet.scala
+++ src/main/scala/uk.co.bruntonspall.scalecamp/servlets/DispatcherServlet.scala
@@ -2,11 +2,32 @@ package uk.co.bruntonspall.scalecamp.servlets
 
 import org.scalatra.ScalatraServlet
 import uk.co.bruntonspall.scalecamp.scalatra.TwirlSupport
+import uk.co.bruntonspall.scalecamp.utils.Config
 
 class DispatcherServlet extends ScalatraServlet with TwirlSupport {
 
   get("/") {
-    html.welcome.render("First paragraph" :: "Second Paragraph" :: Nil)
+  	if (Config.get("is.setup").isEmpty)
+  	  redirect("/setup")
+    Config.put("key", "value")
+    println(Config.get("key"))
+    html.welcome.render
   }
 
+get("/setup") {
+	Config.get("is.setup") match {
+		case "1" => redirect("/")
+		case _ => html.setup.render(Config.configs)
+	}
+}
+post("/setup") {
+	Config.get("is.setup") match {
+		case "1" => redirect("/")
+		case _ => {
+      params.foreach { case(k,v) => Config.put(k,v) }
+      Config.put("is.setup", "1")
+      redirect("/")
+		}
+	}	
+}
 }
--- src/main/scala/uk.co.bruntonspall.scalecamp/utils/Config.scala
+++ src/main/scala/uk.co.bruntonspall.scalecamp/utils/Config.scala
@@ -1,10 +1,38 @@
 package uk.co.bruntonspall.scalecamp.utils
 
-import com.typesafe.config.ConfigFactory
+
+import com.googlecode.objectify.annotation.Unindexed
+import com.googlecode.objectify.{NotFoundException, Key, ObjectifyService}
+import javax.persistence.Id
+
+class ConfigValue {
+  @Id var id:String = null
+  @Unindexed var value:String = ""
+}
 
 object Config {
-  lazy val conf = ConfigFactory.load()
-  lazy val twitter_consumer_key = conf.getString("oauth.twitter.consumer_key")
-  lazy val twitter_consumer_secret = conf.getString("oauth.twitter.consumer_secret")
-  lazy val twitter_callback = conf.getString("oauth.twitter.callback")
+  ObjectifyService.register(classOf[ConfigValue])
+
+  def get(key:String):String =
+    Option(ObjectifyService.begin().find(classOf[ConfigValue], key)) match {
+      case Some(cv) => cv.value
+      case None => configs.getOrElse(key, "")
+    }
+
+  def put(key:String, value:String):Key[ConfigValue] = {
+    val cf = new ConfigValue()
+    cf.id = key
+    cf.value = value
+    ObjectifyService.begin().put(cf)
+  }
+
+  val twitter_consumer_key = "oauth.twitter.consumer_key"
+  val twitter_consumer_secret = "oauth.twitter.consumer_secret"
+  val twitter_callback = "oauth.twitter.callback"
+
+  val configs = Map(
+    twitter_consumer_key -> "",
+    twitter_consumer_secret -> "",
+    twitter_callback -> ""
+    )
 }
